741316db80b7b323794a473bf8102b0efac69114
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-07 16:06:22 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -21,7 +21,7 @@ site = st.sidebar.text_input("Add Your Website", max_chars=None, label_visibilit
 
 if site is not None:
 	crawl(site, 'crawl.jl', follow_links=True)
-if site is None:
+else:
 	break
 
 crawl_df = pd.read_json('crawl.jl', lines=True)
