f67813c68e1d672a282aa164e6efd35e1f8ec4f3
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-07 14:42:01 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -4,7 +4,6 @@ from advertools import crawl
 import pandas as pd
 from collections import OrderedDict
 st.set_page_config(layout="wide")
-
 st.sidebar.title('Internal Links Tool')
 st.sidebar.subheader('Add Your URL')
 site = st.sidebar.text_input("Add Your Website", max_chars=None, label_visibility="visible")
@@ -19,6 +18,24 @@ new_df['links_url'] = new_df['links_url'].str.replace('@@',', ')
 new_df = new_df.dropna(subset=['links_url'])
 new_df['links_url'] = new_df['links_url'].str.split().apply(lambda x: OrderedDict.fromkeys(x).keys()).str.join(' ')
 new_df['links on page'] = new_df['links_url'].str.split().str.len()
+inlinks = new_df.links_url.str.split(expand=True).stack().value_counts()
+inlinks_df = inlinks.to_frame()
+inlinks_df.reset_index(inplace=True)
+inlinks_df = inlinks_df.replace(',','', regex=True)
+inlinks_df.columns = ["url","total_inlinks"]
+inlinks_df = inlinks_df[inlinks_df["url"].str.contains(site)]
+inlinks_df.sort_values(by=['total_inlinks'])
+results = inlinks_df.merge(new_df)
+results = results.drop_duplicates(subset=['url'])
+rows = len(results.axes[0])
 
+inlink_score = []
 
-st.table(new_df)
+# Loop items in results
+for link in results['total_inlinks']:
+  score = link/rows
+  inlink_score.append(score)
+  
+results["inlink score"] = inlink_score
+
+st.table(results)
