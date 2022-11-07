ff95ecda454917c798b38e932011194971ce680f
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-07 14:37:38 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -1,9 +1,9 @@
-import streamlit as st
 import advertools as adv
 from advertools import crawl
 import pandas as pd
+from collections import OrderedDict
 
-st.sidebar.title("Internal Link Tool", anchor=None)
+st.sidebar.title(body, anchor=None)
 st.sidebar.subheader('Add Your URL')
 site = st.sidebar.text_input("Add Your Website", max_chars=None, label_visibility="visible")
 
@@ -11,4 +11,12 @@ if site is not None:
 	crawl(site, 'crawl.jl', follow_links=True)
 
 crawl_df = pd.read_json('crawl.jl', lines=True)
-st.table(crawl_df)
+
+new_df = crawl_df[['url','title','links_url']]
+new_df['links_url'] = new_df['links_url'].str.replace('@@',', ')
+new_df = new_df.dropna(subset=['links_url'])
+new_df['links_url'] = new_df['links_url'].str.split().apply(lambda x: OrderedDict.fromkeys(x).keys()).str.join(' ')
+new_df['links on page'] = new_df['links_url'].str.split().str.len()
+
+
+st.table(new_df)
