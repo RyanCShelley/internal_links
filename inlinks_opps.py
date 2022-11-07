4b25a49cc90e257eefcedd35b13552000b960c54
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-07 16:22:59 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -14,6 +14,11 @@ nltk.download('punkt')
 from rake_nltk import Rake
 rake_nltk_var = Rake()
 import trafilatura
+import re
+import nltk.corpus
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+
 
 st.set_page_config(layout="wide")
 st.sidebar.title('Internal Links Tool')
@@ -25,7 +30,7 @@ if site is not None:
 
 crawl_df = pd.read_json('crawl.jl', lines=True)
 
-new_df = crawl_df[['url','title','links_url']]
+new_df = crawl_df[['url','title','links_url','body']]
 new_df['links_url'] = new_df['links_url'].str.replace('@@',', ')
 new_df = new_df.dropna(subset=['links_url'])
 new_df['links_url'] = new_df['links_url'].str.split().apply(lambda x: OrderedDict.fromkeys(x).keys()).str.join(' ')
@@ -50,7 +55,7 @@ for link in results['total_inlinks']:
   
 results["inlink score"] = inlink_score
 results = results.sort_values(by='inlink score', ascending=False)
-results = results[['url', 'title', 'links on page', 'total_inlinks', 'inlink score', 'links_url']]
+results = results[['url', 'title', 'links on page', 'total_inlinks', 'inlink score', 'body','links_url']]
 
 st.header('Crawl Data')
 st.dataframe(results)
@@ -86,10 +91,12 @@ results["keyword"] = nltk_terms
 
 results['keyword'] = [','.join(map(str, l)) for l in results['keyword']]
 results.keyword.str.split(expand=True).stack().value_counts()
+
 keywords = results.keyword.str.split(expand=True).stack().value_counts()
 keywords = keywords.to_frame()
 keywords = keywords.reset_index()
 keywords.columns = ["Keyword", "Frequnecy"]
+
 fig = px.bar(keywords, x='Keyword', y='Frequnecy')
 fig.show()
 
@@ -103,25 +110,14 @@ col2.dataframe(keywords)
 
 st.header('Finding URLs with Low Inlink Score')
 
-content = []
-
-# Loop items in results
-for page in results['url']:
-  downloaded = trafilatura.fetch_url(page)
-  if downloaded is not None: # assuming the download was successful
-    content.append(trafilatura.extract(downloaded, include_tables=False, include_formatting=False, include_comments=False))
-  else:
-   content.append("No Text")
-
-results["content"] = content
 
 results = results.dropna()
 stop_words = stopwords.words('english')
-results['content'] = results['content'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
+results['body'] = results['body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
 
-results.content = results.apply(lambda row: re.sub(r"http\S+", "", row.content).lower(), 1)
-results.content = results.apply(lambda row: " ".join(filter(lambda x:x[0]!="@", row.content.split())), 1)
-results.content = results.apply(lambda row: " ".join(re.sub("[^a-zA-Z]+", " ", row.content).split()), 1)
+results.body = results.apply(lambda row: re.sub(r"http\S+", "", row.body).lower(), 1)
+results.body = results.apply(lambda row: " ".join(filter(lambda x:x[0]!="@", row.body.split())), 1)
+results.body = results.apply(lambda row: " ".join(re.sub("[^a-zA-Z]+", " ", row.body).split()), 1)
 
 
 score = st.slider('Set Max Inlink Score', 0, 1, 10)
@@ -129,5 +125,4 @@ score = st.slider('Set Max Inlink Score', 0, 1, 10)
 
 inlink_opps_score = results[results['inlink score'] < score]
 
-col2.dataframe(inlink_opps_score)
-
+st.dataframe(inlink_opps_score)
