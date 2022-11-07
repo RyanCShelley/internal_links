a944387ff22dfbc7d0a3f80510c0e9408a92fc34
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-07 15:54:55 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -87,6 +87,8 @@ results['keyword'] = [','.join(map(str, l)) for l in results['keyword']]
 results.keyword.str.split(expand=True).stack().value_counts()
 keywords = results.keyword.str.split(expand=True).stack().value_counts()
 keywords = keywords.to_frame()
+keywords = keywords.reset_index()
+keywords.columns = ["Keyword", "Frequnecy"]
 
 st.header('Keyword Frequency')
 st.dataframe(keywords)
