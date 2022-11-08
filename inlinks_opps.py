03a3bc48cebc123bff9706152d5e54a9ce3da288
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-08 16:02:18 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -19,31 +19,6 @@ import nltk.corpus
 nltk.download("stopwords")
 from nltk.corpus import stopwords
 
-def add_logo():
-    st.markdown(
-        """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url(http://placekitten.com/200/200);
-                background-repeat: no-repeat;
-                padding-top: 120px;
-                background-position: 20px 20px;
-            }
-            [data-testid="stSidebarNav"]::before {
-                content: "My Company Name";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 30px;
-                position: relative;
-                top: 100px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-add_logo() 
-
 
 
 st.set_page_config(layout="wide")
