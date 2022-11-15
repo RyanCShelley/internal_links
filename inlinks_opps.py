52dd9703d461e07f6c132e700c35c233ef1880a2
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-15 16:00:58 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -69,7 +69,7 @@ else:
 	results.body_text = results.apply(lambda row: " ".join(filter(lambda x:x[0]!="@", row.body_text.split())), 1)
 	results.body_text = results.apply(lambda row: " ".join(re.sub("[^a-zA-Z]+", " ", row.body_text).split()), 1)
 
-	max_score = results["inlink score"].quantile(q=0.1, interpolation='linear')
+	max_score = results["inlink score"].quantile(q=0.90, interpolation='linear')
 	inlink_opps_score = results[results['inlink score'] <= max_score]
 	inlink_opps_score = inlink_opps_score[~inlink_opps_score['url'].str.contains('category')]
 	inlink_opps_score = inlink_opps_score[~inlink_opps_score['url'].str.contains('author')]
