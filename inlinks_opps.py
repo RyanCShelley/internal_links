fd5afae63e0f2ced60d4f01d4888503ebd753b71
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-11-15 13:24:36 -0500

    Update inlinks_opps.py

--- inlinks_opps.py
+++ inlinks_opps.py
@@ -59,6 +59,9 @@ else:
 	results = results.sort_values(by='inlink score', ascending=False)
 	results = results[['url', 'title', 'links on page', 'total_inlinks', 'inlink score', 'body_text','links_url']]
 	
+	@st.cache
+	def cache_df(results):
+		return results
 		
 	st.header('Step 1: Review Crawl Data')
 	st.dataframe(results, use_container_width=True)
